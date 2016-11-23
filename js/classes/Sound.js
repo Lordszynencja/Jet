032e0b03bc2b23d460995528dd0ae3ed11af0953
Tamamo <lordszynencja@gmail.com>
2016-11-23 21:52:32 +0100

    music added

--- js/classes/Sound.js
+++ js/classes/Sound.js
@@ -1,11 +1,29 @@
 class Sound {
-	play(x,v) {
+	defaultOnEnded(id) {
+		return function() {
+			delete s.playing[id];
+		};
+	}
+	
+	play(name, volume) {
 		this.active_sound++;
 		var a = this.active_sound;
-		this.playing[a] = new Audio("sounds/"+this.sounds[x]);
-		this.playing[a].onended = function() { delete s.playing[a]; };
-		this.playing[a].volume = v*this.overall_volume;
+		this.playing[a] = new Audio("sounds/"+this.sounds[name]);
+		this.playing[a].onended = this.defaultOnEnded(a);
+		this.playing[a].volume = volume*this.overallVolume;
 		this.playing[a].play();
+	}
+	
+	playMusic(music) {
+		if (this.music == null) {
+			this.music = new Audio("music/" + this.musics[music]);
+			this.music.volume = conf.musicVolume*this.overallVolume;
+			this.music.loop = true;
+		}
+	}
+	
+	setMusicVolume(volume) {
+		
 	}
 
 	prepare() {
@@ -15,12 +33,18 @@ class Sound {
 	constructor() {
 		this.active_sound = 0;
 		this.playing = {};
-		this.overall_volume = 1;
+		this.overallVolume = 1;
 		this.prepare();
 		this.sounds = {
-			shot:"shot3.mp3",
-			laser:"laser.mp3",
-			gameover:"game_over.mp3"
+			shot: "shot3.mp3",
+			laser: "laser.mp3",
+			gameover: "game_over.mp3"
 		};
+		
+		this.musics = {
+			
+		}
+		
+		this.music = null;
 	}
 }
