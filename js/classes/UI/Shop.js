cf09cc7f3e72469076d17f1372d7cac62db650bc
Tamamo <lordszynencja@gmail.com>
2017-01-19 13:18:27 +0100

    shop menu class created

--- js/classes/UI/Shop.js
+++ js/classes/UI/Shop.js
@@ -1,54 +1,68 @@
-class PauseMenu {
+class Shop {
 	onPress(name) {
 		if (name=='esc') {
 			delete ui.menu;
 			ui.newMenu(new LevelSelectMenu(this.level));
 		} else if (name=='enter') {
-			if (this.position == 0) {
-				delete ui.menu;
-				ui.newMenu(new Interface(this.level));
-				s.setMusicVolume(1);
-			} else {
-				delete ui.menu;
-				ui.newMenu(new LevelSelectMenu());
-				g.setInvertion([0, 0], 0);
-				s.changeMusic('menu');
-			}
-		} else if (this.position<this.options.length-1 && name=='down') {
+			this['enter'+this.position]();
+		} else if (name=='down' && this.position<this.options.length-1) {
 			this.position++;
-		} else if (this.position>0&& name=='up') {
+		} else if (name=='up' && this.position>0) {
 			this.position--;
 		}
+	}
+	
+	enter0() {
+		//change ship
+	}
+	
+	enter1() {
+		//upgrade ship
+	}
+	
+	enter2() {
+		//manage weapons
+	}
+	
+	enter3() {
+		//shop
+	}
+	
+	enter4() {
+		//test flight
+	}
+	
+	enter5() {
+		delete ui.menu;
+		ui.newMenu(new LevelSelectMenu(this.level));
 	}
 	
 	anyKey() {
 	}
 	
 	update() {
+		p.x = -0.5;
+		p.y = 0;
+		p.ship.x = p.x;
+		p.ship.y = p.y;
 	}
 	
 	draw() {
-		g.addBackgroundTexture(this.level.texture, makeCoords2(1,1));
+		g.addBackgroundTexture(this.level.texture, makeCoords2(1, 1));
 		p.draw();
 		for (var i in effects) effects[i].draw();
-		for (var i in enemies) enemies[i].draw();
-		for (var i in playerMissiles) playerMissiles[i].draw();
-		for (var i in enemyMissiles) enemyMissiles[i].draw();
-		drawStandardHp();
-		drawStandardHeat();
-		g.drawText(-0.96, -0.8, stats.score.toString(), 0.1, [0.1, 0.1, 0.1, 1]);
-		this.drawOptions();
+		for (var i=0;i<this.options.length;i++) {
+			var xy = this.optionsV[i];
+			g.drawText(xy[0], xy[1], this.options[i], this.fontSize, [1, 1, 1, 1]);
+		}
+		g.drawText(-0.95, -0.9, 'Money: '+stats.money.toString()+'$', 0.05, [1, 1, 1, 1]);
 	}
 	
 	constructor() {
-		this.
 		this.position = 0;
-		this.heatBgV = makeCoords4(0.1,0.5,-0.95,-0.85);
-		this.heatCriticalV = makeCoords4(0.4,0.5,-0.95,-0.85);
-		this.lifeBgV = makeCoords4(0.55,0.95,-0.95,-0.85);
+		this.fontSize = 0.055;
 		
-		this.options = ['Continue','Exit'];
-		this.optionsV = prepareOptionsVertexes(this.options.length);
-		
+		this.options = ['Change Ship', 'Upgrade Ship', 'Manage Weapons', 'Weapons Shop', 'Test Flight', 'Exit'];
+		this.optionsV = prepareOptionsPositions(this.options, this.fontSize);
 	}
 }
